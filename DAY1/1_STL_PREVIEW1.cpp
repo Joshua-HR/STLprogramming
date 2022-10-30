128d49bf0025e0e3fc6146bee58788c51f19739b
ecourse <smkang@codenuri.co.kr>
2022-10-31 08:56:28 +0900

    Add files via upload

---
+++ DAY1/1_STL_PREVIEW1.cpp
@@ -0,0 +1,22 @@
+// 1_STL_PREVIEW1
+#include <iostream>
+#include <string>
+
+int main()
+{
+	char s1[] = "Hello";
+	char s2[] = "Hello";
+
+	if (s1 == s2) {}
+	
+//	s2 = s1;
+}
+// Visual studio 에서 Day1 프로젝트 열어 보세요
+
+// 1. 버전 설정해야 합니다.
+// => 프로젝트 메뉴 선택후, Day1 속성 메뉴 선택
+// => 플랫폼 도구 집합, Windows SDK 버전 설정하시면 됩니다.
+
+// 2. 소스 코드를 빌드에 포함/제거 하는 방법
+
+// 3. 빌드하는 방법 - "Ctrl + F5"

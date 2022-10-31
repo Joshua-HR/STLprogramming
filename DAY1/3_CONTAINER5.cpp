6acbae30d54d8c5c6f8af7bbabe577ed9cc9f4bf
ecourse <smkang@codenuri.co.kr>
2022-10-31 11:49:11 +0900

    Add files via upload

--- DAY1/3_CONTAINER5.cpp
+++ DAY1/3_CONTAINER5.cpp
@@ -12,11 +12,17 @@
 
 int main()
 {
-	std::vector<int> c = { 1,2,3,4,5,6,7,8,9,10 }; 
+//	std::vector<int> c = { 1,2,3,4,5,6,7,8,9,10 }; 
+	std::list<int>   c = { 1,2,3,4,5,6,7,8,9,10 };
 
 	// 뒤집는 알고리즘은 std::reverse 입니다. 뒤집어 보세요
-	// ??
+	// 핵심 : 한개의 std::reverse 를 가지고 , 대부분의 컨테이너에 사용가능
+	std::reverse(c.begin(), c.end());
 
 	for (int e : c)
 		std::cout << e << std::endl;
 }
+// STL의 3대 요소
+// Container  : 3일차
+// Iterator   : 1일차
+// Algorithm  : 2일차

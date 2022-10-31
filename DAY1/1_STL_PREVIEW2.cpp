6052a5f219cbec841653a14c81b75f1a2d3f08cf
ecourse <smkang@codenuri.co.kr>
2022-10-31 09:23:14 +0900

    Add files via upload

---
+++ DAY1/1_STL_PREVIEW2.cpp
@@ -0,0 +1,22 @@
+// 1_STL_PREVIEW1
+#include <iostream>
+#include <list>
+
+// C++ 표준 라이브러리인 STL 에는 대부분의 자료구조 클래스가 제공됩니다
+
+// STL 변화
+// C++98 : 자료구조와 알고리즘 위주의 라이브러리. ( 1일차 ~ 3일차 )
+// C++11 : 추가 자료구조와 알고리즘
+//         다양한 유틸리티 클래스(스마트포인터, function, 멀티스레드)
+//         (4~5일차)
+
+int main()
+{
+	std::list<int> s;
+	s.push_back(10);
+	s.push_back(20);
+
+}
+
+
+

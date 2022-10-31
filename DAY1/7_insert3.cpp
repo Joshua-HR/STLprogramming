557a8ba45d5336685fffca43c3dcbb47522a8f97
ecourse <smkang@codenuri.co.kr>
2022-10-31 16:44:51 +0900

    Add files via upload

--- DAY1/7_insert3.cpp
+++ DAY1/7_insert3.cpp
@@ -9,12 +9,19 @@ int main(int argc, char** argv)
 	std::list<int> s = {    0, 0, 0, 0, 0     };
 
 
-	// 삽입반복자는 3가지가 있습니다.
+	// 핵심 1.  삽입반복자는 3가지가 있습니다.
 	// 전방 삽입, 후방 삽입, 임의 삽입
 	// 임의 삽입의 경우, 어느 위치에 넣을지 반복자를 전달해야 합니다.
-	std::front_insert_iterator< std::list<int> > p1(s);
-	std::back_insert_iterator< std::list<int> >  p2(s);
-	std::insert_iterator< std::list<int> > p3(s, ++s.begin());
+
+//	std::front_insert_iterator< std::list<int> > p1(s);
+//	std::back_insert_iterator< std::list<int> >  p2(s);
+//	std::insert_iterator< std::list<int> > p3(s, ++s.begin());
+
+	// 핵심 2. 삽입반복자 만들때 함수 버전을 사용하는 것이 편리합니다.
+	auto p1 = std::front_inserter(s);
+	auto p2 = std::back_inserter(s);
+	auto p3 = std::inserter(s, ++s.begin());
+
 
 	*p1 = 10; // s.push_front(10)
 	*p2 = 20; // s.push_back(20);

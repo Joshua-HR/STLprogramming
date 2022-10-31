50f3ed1779d00e98c9512d9569554eaf621d462c
ecourse <smkang@codenuri.co.kr>
2022-10-31 09:35:22 +0900

    Add files via upload

---
+++ DAY1/2_STL_ALGORITHM2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// 2. 
+
+
+char* xstrchr(char* first, char* last, int c)
+{
+	while (first != last && *first != c)
+		++first;
+
+	return first == last ? nullptr : first;
+}
+
+int main()
+{
+	char s[] = "abcdefgh";
+
+	char* p = xstrchr(s, s+4, 'c');
+
+
+
+	if (p == nullptr)
+		std::cout << "not found" << std::endl;
+	else
+		std::cout << "found : " << *p << std::endl;
+}

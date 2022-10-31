cff57d5d1d5dc8fcc3acbd8be15e01493248b041
ecourse <smkang@codenuri.co.kr>
2022-10-31 16:51:26 +0900

    Add files via upload

--- DAY1/7_insert3-1.cpp
+++ DAY1/7_insert3-1.cpp
@@ -9,8 +9,15 @@ int main(int argc, char** argv)
 
 
 	// 결과 예측해 보세요
-	std::copy(s1.begin(), s1.end(), std::back_inserter(s2));
-		// => 
+//	std::copy(s1.begin(), s1.end(), std::back_inserter(s2));
+		// => 0, 0, 0, 0, 0, 1, 2, 3, 4, 5
+
+//	std::copy(s1.begin(), s1.end(), std::front_inserter(s2));
+		// => 5, 4, 3, 2, 1, 0, 0, 0, 0, 0
+
+
+	std::copy(s1.begin(), s1.end(), std::inserter(s2, s2.begin()));
+		// => 1, 2, 3, 4, 5, 0, 0, 0, 0, 0
 
 	for (auto& n : s2)
 		std::cout << n << ", ";

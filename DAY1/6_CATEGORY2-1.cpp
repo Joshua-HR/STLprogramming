4c28c6154f0802c996e6363a9c96e75c04b133bd
ecourse <smkang@codenuri.co.kr>
2022-10-31 15:22:43 +0900

    Add files via upload

--- DAY1/6_CATEGORY2-1.cpp
+++ DAY1/6_CATEGORY2-1.cpp
@@ -11,7 +11,18 @@ int main()
 	std::vector<int> v = { 1,2,3,4 }; // 임의접근 반복자를 가짐
 	std::list<int> s   = { 1,2,3,4 }; // 양방향 반복자를 가짐
 
+
 	// 핵심 2. 각 알고리즘이 요구하는 반복자의 종류를 알아 두세요
+	// => std::sort() 는 임의 접근 반복자를 요구 합니다.
 	std::sort(v.begin(), v.end());
-//	std::sort(s.begin(), s.end());
+//	std::sort(s.begin(), s.end()); // error
+		// => 알고리즘은 "임의접근 반복자"를 요구
+		// => list 반복자는 "양방향 반복자" 이므로 조건 충족 안됨
+
+	s.sort(); // ok.. 
+
+//	std::sort()
+
+	// cppreference.com 에 접속해 보세요
+	// 1번째 화면에서 "Algorithm Library" 찾아서 선택하세요
 }

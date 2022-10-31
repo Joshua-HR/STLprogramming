53edada0d236a0496e93555563315c62011fbefa
ecourse <smkang@codenuri.co.kr>
2022-10-31 09:38:48 +0900

    Add files via upload

--- DAY1/2_STL_ALGORITHM2.cpp
+++ DAY1/2_STL_ALGORITHM2.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
-// 2. 
 
+// 2. 검색 구간의 일반화(활용도가 넓어지고 있다는 의미) 
+
+// => 전체 문자열 뿐 아니라 부분 문자열도 검색 가능하게 하자.
+// => last 는 검색 대상 아님. 
+//    [first, last) 로 표기. 반 개행 구간(half open range)
 
 char* xstrchr(char* first, char* last, int c)
 {
@@ -15,7 +19,8 @@ int main()
 {
 	char s[] = "abcdefgh";
 
-	char* p = xstrchr(s, s+4, 'c');
+//	char* p = xstrchr(s, s+4, 'c');
+	char* p = xstrchr(s, s + 4, 'e'); // 검색 실패
 
 
 

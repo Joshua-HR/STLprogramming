04cc5cc6b25c36daf4e12f791956188d5cd3d89c
ecourse <smkang@codenuri.co.kr>
2022-11-01 08:59:15 +0900

    Add files via upload

---
+++ DAY2/1_stream5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <fstream> // C++ 파일 입출력 헤더.
+
+int main()
+{
+	std::ifstream fin("1_stream5.cpp"); 
+
+	std::istream_iterator<char> p1(fin);
+	std::istream_iterator<char> p2;     // 생성자 인자를 전달하지 않으면
+										// EOF 입니다.
+
+	std::ostream_iterator<char> p3(std::cout, "");
+
+	
+	// 아래 한줄의 의미를 생각해 보세요
+	std::copy(p1, p2, p3);
+}
+

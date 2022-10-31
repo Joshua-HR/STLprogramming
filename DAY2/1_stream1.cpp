4c7b709d040ccfb5a4711247dc828390c11eea65
ecourse <smkang@codenuri.co.kr>
2022-11-01 08:41:00 +0900

    Add files via upload

--- DAY2/1_stream1.cpp
+++ DAY2/1_stream1.cpp
@@ -12,15 +12,21 @@ int main()
 
 
 	// 2. ostream_iterator 를 사용하는 방법
+	// => 출력을 할때 사용하는 반복자
 
 	std::ostream_iterator<int> p(std::cout, " ");
 	
 	*p = 10; // std::cout << 10 << " "; 가 같은 코드
+	*p = 20; 
 
-
-
+	++p; // ostream_iterator 는 ++ 이 하는 일은 없습니다.
+		 // 하지만 모든 반복자는 ++ 연산을 지원해야 합니다.
 
 
 	std::list<int> s = { 1,2,3 };
+
+	// 출력 반복자 덕분에 컨테이너의 모든 요소를 출력할때
+	// std::copy 같은 것을 사용해서 한줄로 할수 있습니다.
+	std::copy(s.begin(), s.end(), p);
 }
 

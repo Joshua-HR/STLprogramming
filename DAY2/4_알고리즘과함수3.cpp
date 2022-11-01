004010686703ba75f2da9d3e978a9cc2e49e9d08
ecourse <smkang@codenuri.co.kr>
2022-11-01 11:45:19 +0900

    Add files via upload

---
+++ DAY2/4_알고리즘과함수3.cpp
@@ -0,0 +1,18 @@
+// 4_알고리즘과함수3   "3_algo1-1.cpp" 복사
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,1,2,3,1,2,3,1 };
+
+	auto p1 = std::find(v.begin(), v.end(), 3);
+
+}
+
+
+
+

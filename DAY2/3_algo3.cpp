ffd2dd8ed84c6749dc9ac989949e25ef5e6b3a28
ecourse <smkang@codenuri.co.kr>
2022-11-01 11:05:43 +0900

    Add files via upload

---
+++ DAY2/3_algo3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include "show.h"
+
+// 핵심 : 어떤 컨테이너가 알고리즘(일반함수)과 
+//       동일한 이름의 멤버 함수가 있다면 멤버 함수를 사용하라.
+
+// 왜 알고리즘과 동일한 이름의 멤버함수를 별도로 제공하나요 ?
+
+// 이유 1. 더 최적화된 구현이 가능할때
+//		  => list.remove(3);  // 알고리즘 remove 보다 
+//							  // list remove 가 좋다.!
+// 
+// 이유 2. 알고리즘으로 보낼수 없을때
+//		  => std::sort( list.begin(), list.end()) // error
+//        => list.sort()   // ok
+
+
+
+int main()
+{
+//	std::vector<int> v = { 1,2,3,1,2,3,1,2,3,1 };
+	std::list<int> v = { 1,2,3,1,2,3,1,2,3,1 };
+
+	// 아래 4줄은 에러는 아닙니다.
+	// 그런데, list 인 경우에는 "3"을 만날때 마다 바로 삭제 하는것이
+	// 좋습니다.
+	/*
+	auto p = std::remove(v.begin(), v.end(), 3);
+	show(v);
+	v.erase(p, v.end()); 
+	show(v);
+	*/
+
+	// 그래서, list 에는 별도의 "remove"라는 멤버 함수가 있습니다.
+	v.remove(3); // 3을 만날때 마다 제거!
+
+	show(v);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+

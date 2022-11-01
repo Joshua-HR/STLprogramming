a2a127b590dd6d86a26cb36a5d5544ebd0a996a8
ecourse <smkang@codenuri.co.kr>
2022-11-01 11:41:06 +0900

    Add files via upload

--- DAY2/4_알고리즘과함수2.cpp
+++ DAY2/4_알고리즘과함수2.cpp
@@ -3,6 +3,11 @@
 #include <algorithm>
 #include "show.h"
 
+// 아래 코드의 transform 의 마지막 인자는 이항 함수 이어야 합니다.
+int foo(int a, int b)
+{
+    return a + b;
+}
 
 int main()
 {
@@ -12,12 +17,15 @@ int main()
 
     // transform 알고리즘
     // => 사용법 복잡합니다. 잘 이해해 보세요
+    // => 아래 코드를 보고 "foo" 함수의 모양(반환타입, 인자)를 예측해보세요
 
     std::transform(s1.begin(), s1.end(), // 이 구간의 요소를
                    s2.begin(),  // 이 구간의 요소와 함께 foo함수로
-                   s3.begin(),  // foo 함수의 결과를 이 구간에
+                   //s3.begin(),  // foo 함수의 결과를 이 구간에
+                   std::back_inserter(s3), 
                    foo);        // 
 
+    show(s3);
 	return 0;
 }
 

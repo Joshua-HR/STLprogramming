631a34c47119d9fc8faab9c1ee63e30c399c17da
ecourse <smkang@codenuri.co.kr>
2022-11-01 09:03:23 +0900

    Add files via upload

--- DAY2/1_stream5.cpp
+++ DAY2/1_stream5.cpp
@@ -7,9 +7,14 @@ int main()
 {
 	std::ifstream fin("1_stream5.cpp"); 
 
-	std::istream_iterator<char> p1(fin);
-	std::istream_iterator<char> p2;     // 생성자 인자를 전달하지 않으면
+	// std::istream_iterator : white space(space, tab, enter)무시
+//	std::istream_iterator<char> p1(fin);
+//	std::istream_iterator<char> p2;     // 생성자 인자를 전달하지 않으면
 										// EOF 입니다.
+
+	// std::istreambuf_iterator : white space 를 입력에 포함
+	std::istreambuf_iterator<char> p1(fin);
+	std::istreambuf_iterator<char> p2;
 
 	std::ostream_iterator<char> p3(std::cout, "");
 

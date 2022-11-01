25479bfd068c949404509fbe2296b37a15728413
ecourse <smkang@codenuri.co.kr>
2022-11-01 09:13:47 +0900

    Add files via upload

--- DAY2/1_stream6.cpp
+++ DAY2/1_stream6.cpp
@@ -18,16 +18,18 @@ void show(const Container& c)
 // 2. 반복자 버전
 // => 반복자는 크기가 작은 타입입니다. 
 //    "call by value" 를 사용해도 나쁘지 않습니다.
+// Container::value_type  => "컨테이너가 저장하는 타입"
+// Iterator::value_type  => "반복자가 가리키는 타입"
+
 template<typename Iter>
 void show(Iter first, Iter last)
 {
-	std::ostream_iterator< ? > p(std::cout, ", ");
+	std::ostream_iterator< typename Iter::value_type > p(std::cout, ", ");
 
 	std::copy(first, last, p);
 
 	std::cout << std::endl;
 }
-
 
 int main()
 {

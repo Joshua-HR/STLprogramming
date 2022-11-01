716dccbae78379221b4472b4d35f69f41840f2ff
ecourse <smkang@codenuri.co.kr>
2022-11-01 16:59:38 +0900

    Add files via upload

--- DAY2/10_TRAITS1.cpp
+++ DAY2/10_TRAITS1.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <list>
 
+// 아래 소스의 핵심 
+// typename T::value_type : 반복자 T가 가리키는 요소의 타입
+
+// sum 의 반환 타입을 표기하는 것을 알아 두세요.
+
 template<typename T> 
 typename T::value_type sum(T first, T last)
 {

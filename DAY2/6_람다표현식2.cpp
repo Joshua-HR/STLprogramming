bf7f7dc1c3054da122a8c29782a7061d17821b80
ecourse <smkang@codenuri.co.kr>
2022-11-01 15:56:22 +0900

    Add files via upload

---
+++ DAY2/6_람다표현식2.cpp
@@ -0,0 +1,47 @@
+// 5_함수객체10
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional> 
+#include "show.h"
+
+int main()
+{
+	std::vector<int> v = { 1,3,5,7,9,2,4,6,8,10 };
+
+	// 동일한 람다 표현식이 여러번 사용된다면
+	// 1. auto 변수에 담아서 사용하거나
+	// 2. std::less 등을 사용하는 것이 좋습니다.
+
+	// 아래 코드의 결과는 sort 함수가 3개 생성됩니다.
+	// => 모든 람다 표현식은 다른 타입 입니다.
+	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; });
+									// class xxx{}; xxx();
+
+	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; });
+									// class yyy{}; yyy();
+
+	std::sort(v.begin(), v.end(), [](int a, int b) { return a < b; });
+									// class zzz{}; zzz();
+
+
+	// 위 코드의 해결책1. 람다표현식을 auto 변수에 담아서 sort 에 사용
+	auto cmp = [](int a, int b) { return a < b; };
+				// class kkk{}; kkk();
+	std::sort(v.begin(), v.end(), cmp);
+	std::sort(v.begin(), v.end(), cmp);
+	std::sort(v.begin(), v.end(), cmp);
+
+
+	// 해결책 2. std::less 나 std::greater 같이 이미 만들어진 함수객체사용
+	std::greater<int> f;
+	std::sort(v.begin(), v.end(), f);
+	std::sort(v.begin(), v.end(), f);
+	std::sort(v.begin(), v.end(), f);
+
+}
+
+
+
+
+

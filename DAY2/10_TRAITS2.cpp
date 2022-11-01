62cfe274df0f8032b8f2b0102cd517c67695d605
ecourse <smkang@codenuri.co.kr>
2022-11-01 17:04:45 +0900

    Add files via upload

--- DAY2/10_TRAITS2.cpp
+++ DAY2/10_TRAITS2.cpp
@@ -5,7 +5,9 @@
 template<typename T>
 typename T::value_type sum(T first, T last)
 {
-	typename T::value_type s = 0;
+	// 현재 "T = int*" 입니다. 
+	// 그런데, int* 안에는 value_type 이 없습니다!!
+	typename T::value_type s = 0; // error
 
 	while (first != last)
 	{
@@ -17,14 +19,9 @@ typename T::value_type sum(T first, T last)
 
 int main()
 {
-	std::list<int> s = { 1,2,3,4,5,6,7,8,9,10 };
-	int n = sum(s.begin(), s.end());	
-	std::cout << n << std::endl; 
-
-
 	int x[10] = { 1,2,3,4,5,6,7,8,9,10 };
 
-	// 반복자 : 반복자 처럼 동작하는 모든 것은 반복자 이다.
+	// 반복자 : "반복자 처럼 동작하는 모든 것은 반복자" 이다.
 	//         ++로 이동 가능하고 *로 요소 접근 가능한것
 	//         raw pointer 도 반복자 이다.
 	int n2 = sum(x, x + 10);

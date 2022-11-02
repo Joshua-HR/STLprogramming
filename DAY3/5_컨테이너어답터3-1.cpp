0aaf5f2492af22c3b6a5f20dc0f8d03376979c09
ecourse <smkang@codenuri.co.kr>
2022-11-02 15:47:36 +0900

    Add files via upload

--- DAY3/5_컨테이너어답터3-1.cpp
+++ DAY3/5_컨테이너어답터3-1.cpp
@@ -1,5 +1,6 @@
 // 3_컨테이너어답터3-1
 
+/*
 // 아래 코드에서 컴파일시 에러가 나오는 곳을 찾으세요
 class A
 {
@@ -7,10 +8,26 @@ class A
 public:
 	void foo()
 	{
-		*data = 10;
+		*data = 10;		// error
 	}
 };
 int main()
 {
 	A a;
+}
+*/
+//==================================================
+template<typename T>
+class A
+{
+	T data;
+public:
+	void foo()
+	{
+		*data = 10;		
+	}
+};
+int main()
+{
+	A<int> a;
 }

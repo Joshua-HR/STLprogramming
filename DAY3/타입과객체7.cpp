473f6b478e3b520ba285347e9fbe5a01175775d0
ecourse <smkang@codenuri.co.kr>
2022-11-02 17:16:40 +0900

    Add files via upload

--- DAY3/타입과객체7.cpp
+++ DAY3/타입과객체7.cpp
@@ -18,4 +18,11 @@ int main()
 	auto cmp = [](int a, int b) { return a < b; };
 
 	pqueue< decltype(cmp) > pq(cmp);
+
+	// 단C++20 부터는
+	// 1. 람다 표현식 자체를 decltype 에 넣을수 있고
+	// 2. 람다 표현식이 만드는 타입은 디폴트 생성자도 있습니다.
+	// 그래서 그냥, 아래처럼하면 됩니다.
+
+	pqueue< decltype([](int a, int b) { return a < b; }) > pq1;
 }

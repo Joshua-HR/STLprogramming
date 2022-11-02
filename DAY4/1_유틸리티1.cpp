4189efef6a198505d213ea3bdf9f876b955c0aa3
ecourse <smkang@codenuri.co.kr>
2022-11-03 08:37:03 +0900

    Add files via upload

---
+++ DAY4/1_유틸리티1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "show.h"
+
+template<typename T> void foo(T a) {}
+
+int main()
+{
+	// pair : 서로다른 타입 2개를 보관하는 구조체
+	std::pair<int, double> p1(1, 3.4);
+
+	std::cout << p1.first << std::endl;  // 1
+	std::cout << p1.second << std::endl; // 3.4
+
+	// pair 를 직접 사용 : 타입을 전달해야 하므로 복잡해 보입니다
+	// make_pair 사용   : 타입 생략가능
+
+	auto p2 = std::make_pair(1, 3.4); // 함수 템플릿은 
+									  // 타입인자 생략가능
+
+}

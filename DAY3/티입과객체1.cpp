da5cdf5b1153689079e725245a0e30ddd0f7729a
ecourse <smkang@codenuri.co.kr>
2022-11-02 16:36:34 +0900

    Add files via upload

--- DAY3/티입과객체1.cpp
+++ DAY3/티입과객체1.cpp
@@ -10,10 +10,13 @@ int main()
 	// list: 클래스(템플릿) 입니다.
 
 	// 다음중 잘못된 것을 모두 고르세요 
-	foo(int);
-	foo(n);
+	foo(int);	// error
+	foo(n);		// ok. 함수의 인자는 객체(변수)를 전달합니다.
 
-	list<int> s1;
-	list<n>   s2;
+	list<int> s1; // ok
+	list<n>   s2; // error. 템플릿 인자는 타입을 전달해야 합니다.
+	
+	// 함수 인자 : 타입이 아닌 객체 전달
+	// 템플릿인자 : 타입전달
 }
 

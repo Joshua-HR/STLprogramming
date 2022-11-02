774b0a88c1c16a88d50fed736395b3e3c7395df3
ecourse <smkang@codenuri.co.kr>
2022-11-02 16:53:23 +0900

    Add files via upload

---
+++ DAY3/타입과객체4.cpp
@@ -0,0 +1,29 @@
+#include <stack>
+#include <queue> 
+#include <iostream>
+#include "show.h"
+
+// 절대값 크기로 우선순위를 정하고 싶다.
+// 방법 1. 절대값으로 비교하는 함수 객체를 만드세요
+
+struct abs_less
+{
+	bool operator()(int a, int b) const
+	{
+		return abs(a) < abs(b);
+	}
+};
+
+int main()
+{
+	std::priority_queue<int, std::vector<int>, abs_less > pq;
+
+	pq.push(10);
+	pq.push(-30);
+	pq.push(20);
+
+	std::cout << pq.top() << std::endl;  // -30
+}
+
+
+

89bac92196bd1acf78e4ff459bb3b381f02279b6
ecourse <smkang@codenuri.co.kr>
2022-11-02 14:10:39 +0900

    Add files via upload

---
+++ DAY3/3_선형컨테이너6.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <list>
+#include <string>
+#include <fstream>
+#include "show.h"
+
+// std::cin >> s		     : 한 단어 입력
+// std::getline(std::cin, s) : 한 문장(줄) 입력
+
+int main()
+{
+	std::vector<std::string> v;
+	std::ifstream f("3_선형컨테이너6.cpp"); // 현재 파일이름
+
+	std::string s;
+	while (std::getline(f, s))
+		v.push_back(s);
+	//----------------------------
+
+//	std::reverse(v.begin(), v.end());		// 파일 상/하 뒤집기
+//	std::reverse(v[0].begin(), v[0].end()); // 첫번째 줄 좌우 뒤집기
+
+	for (auto& s : v)
+	{
+		std::reverse(s.begin(), s.end());
+	}
+
+
+	//-------------------------------------
+	for (auto& n : v)
+		std::cout << n << std::endl;
+}
+
+

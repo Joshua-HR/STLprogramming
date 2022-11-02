7f862c71208985c9bb4754aef3c8b849b87bc42f
ecourse <smkang@codenuri.co.kr>
2022-11-02 17:03:58 +0900

    Add files via upload

--- DAY3/타입과객체5.cpp
+++ DAY3/타입과객체5.cpp
@@ -16,10 +16,31 @@ int main()
 					// class xxx{}; xxx();
 	
 	// 방법 2. C++11 decltype 키워드 사용
-	std::priority_queue<int, std::vector<int>, 
-					[](int a, int b) { return abs(a) < abs(b); } > pq;
+	// => 에러
+	// => 람다 표현식 자체는 "평가되지 않은 표현식"에 넣을수 없다.
+	// => decltype() 은 평가되지 않은 표현식 입니다.
+	// int n;
+	// decltype(n) : int
+//	std::priority_queue<int, std::vector<int>, 
+//				decltype([](int a, int b) { return abs(a) < abs(b); }) > pq;
 
 
+
+	// 방법 3. 람다표현식을 auto 변수에 담기
+	// => 에러!
+	// => decltype(cmp) 자체는 문제가 없습니다.
+	// => 이유 : 람다표현식이 만드는 타입은 디폴트 생성자가 없습니다.
+	// => priority_queue 내부에서 멤버 데이타 만들때 디폴트 생성자 요구!
+
+//	auto cmp = [](int a, int b) { return abs(a) < abs(b); };
+
+//	std::priority_queue<int, std::vector<int>,	decltype(cmp) > pq;
+
+
+	// 방법 4. 템플릿 인자로 타입을 전달하고, 생성자로 객체를 전달..!!
+	// 디폴트 생성자가 아닌 복사 생성자를 사용하도록!!!
+	auto cmp = [](int a, int b) { return abs(a) < abs(b); };
+	std::priority_queue<int, std::vector<int>, decltype(cmp) > pq(cmp);
 
 	pq.push(10);
 	pq.push(-30);

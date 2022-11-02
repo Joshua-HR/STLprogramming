faa910d9dc09d9d69c20bdb9435e34a586e0a578
ecourse <smkang@codenuri.co.kr>
2022-11-02 15:33:47 +0900

    Add files via upload

---
+++ DAY3/5_컨테이너어답터2.cpp
@@ -0,0 +1,23 @@
+// Container Adapter
+// => 3개의 sequence container 의 함수이름을 변경해서 만들것
+// 
+// 3개의 컨테이너 어답터가 있습니다.
+#include <stack>	// stack
+#include <queue>	// queue, priority_queue
+#include "show.h"
+
+int main()
+{
+	std::stack<int> s1;
+	s1.push(10);
+	s1.push(20);
+
+	std::cout << s1.top() << std::endl; // 20. 반환만, 제거 안됨
+	std::cout << s1.top() << std::endl; // 20
+
+	s1.pop(); // 제거만 
+
+	std::cout << s1.top() << std::endl; // 10
+	//=======================================================
+
+}

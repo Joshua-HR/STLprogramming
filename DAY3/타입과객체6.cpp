3bec7ec3695771c787c2a91d9ef5b25ea0455abc
ecourse <smkang@codenuri.co.kr>
2022-11-02 17:09:27 +0900

    Add files via upload

--- DAY3/타입과객체6.cpp
+++ DAY3/타입과객체6.cpp
@@ -9,4 +9,10 @@ int main()
 
 	std::cout << typeid(f).name() << std::endl;
 
+
+	// 핵심 2. 람다표현식이 만드는 타입은 디폴트 생성자가 없습니다.
+//	decltype(f) f2;	// error. 
+	// xxx      f2;
+
+	decltype(f) f3 = f;	// ok.. 복사 생성자는 가능.
 }

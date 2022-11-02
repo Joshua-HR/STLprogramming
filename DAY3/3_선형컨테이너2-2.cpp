3df91194e7393603b82246bcd21a636e2ca36057
ecourse <smkang@codenuri.co.kr>
2022-11-02 11:57:49 +0900

    Add files via upload

--- DAY3/3_선형컨테이너2-2.cpp
+++ DAY3/3_선형컨테이너2-2.cpp
@@ -8,6 +8,12 @@ int main()
 	//[] 와 at
 	// [] : 잘못된 인덱스 사용해도 예외 발생안됨. 프로그램 비정상 종료
 	// at : 잘못된 인덱스 사용시 예외 발생
-
-	v.at(10) = 20;
+	try
+	{
+		v.at(10) = 20;
+	}
+	catch (std::out_of_range& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 }

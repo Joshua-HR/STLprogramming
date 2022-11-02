8b5cefb1a254e14622e3d08089436eb04ebad819
ecourse <smkang@codenuri.co.kr>
2022-11-02 11:32:51 +0900

    Add files via upload

---
+++ DAY3/3_선형컨테이너2-1.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include "show.h"
+
+// vector 멤버 함수 정리
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5,6,7,8,9,10 };
+
+	auto p = std::find(v.begin(), v.end(), 5);
+
+	// 1. 삽입, 
+
+
+	// 2. 접근
+
+
+	// 3. 요소 변경
+
+}

a267fab50d776c6d234f754f71cd3897558dc2ba
ecourse <smkang@codenuri.co.kr>
2022-11-02 11:56:41 +0900

    Add files via upload

---
+++ DAY3/3_선형컨테이너2-2.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,4,5 };
+
+	//[] 와 at
+	// [] : 잘못된 인덱스 사용해도 예외 발생안됨. 프로그램 비정상 종료
+	// at : 잘못된 인덱스 사용시 예외 발생
+
+	v.at(10) = 20;
+}

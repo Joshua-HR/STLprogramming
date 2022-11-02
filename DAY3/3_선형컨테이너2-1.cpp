2b3abed4d081a5b6d4bf4fe06587a9e0197d3fcd
ecourse <smkang@codenuri.co.kr>
2022-11-02 11:35:39 +0900

    Add files via upload

--- DAY3/3_선형컨테이너2-1.cpp
+++ DAY3/3_선형컨테이너2-1.cpp
@@ -11,7 +11,13 @@ int main()
 
 	auto p = std::find(v.begin(), v.end(), 5);
 
-	// 1. 삽입, 
+	// 1. 삽입
+//	v.push_front(10); // vector 는 안됨
+	v.push_back(20);
+
+	v.insert(p, 30); // p 가 가리키는 요소 앞에 30 넣기
+
+	show(v); // 1,2,3,4,30,5,6,7,8,9,10,20
 
 
 	// 2. 접근

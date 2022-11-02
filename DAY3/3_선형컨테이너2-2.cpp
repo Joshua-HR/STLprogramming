6a670c165f7bb5b259eebce800ded8cfa7b6c090
ecourse <smkang@codenuri.co.kr>
2022-11-02 12:00:17 +0900

    Add files via upload

--- DAY3/3_선형컨테이너2-2.cpp
+++ DAY3/3_선형컨테이너2-2.cpp
@@ -10,10 +10,19 @@ int main()
 	// at : 잘못된 인덱스 사용시 예외 발생
 	try
 	{
-		v.at(10) = 20;
+	//	v.at(10) = 20;
+	//	v[10] = 20;   // 내부적으로 index 확인도 안합니다.
+					  // 그냥 시작주소[10] = 20 입니다.
 	}
 	catch (std::out_of_range& e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << "예외 : " << e.what() << std::endl;
+	}
+	//====================================
+	for (int i = 0; i < v.size(); i++)
+	{
+		// 다음중 좋은 코드는 ?
+		v.at(i) = 0; // 1
+		v[i]    = 0; // 2
 	}
 }

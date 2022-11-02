46fa8e2343c6d29acb1f3287f6cdabbc018a191c
ecourse <smkang@codenuri.co.kr>
2022-11-02 10:04:57 +0900

    Add files via upload

--- DAY3/2_컨테이너의공통의특징.cpp
+++ DAY3/2_컨테이너의공통의특징.cpp
@@ -11,23 +11,27 @@ public:
 	// 2. 멤버 함수
 
 	// 3. 멤버 타입 - STL 컨테이너(반복자)의 특징
-	using value_type = T;
-
+	using value_type = T; // typedef T value_type
+	using size_type = std::size_t; // unsigned int
+	using iterator = list_iterator<T>;
 };
-
 
 int main()
 {
 	// STL 컨테이너의 특징
 	std::list<int> s = { 1,2,3 };
 
-
 	// 1. 멤버 타입이 있다.
 	std::list<int>::value_type n = s.front();
+					    // int n = s.front();
 
+	// ? 에 들어갈 "최선의 코드"를 생각해 보세요
+	unsigned int sz = s.size();
+				// => 이 코드는 s.size()가 4바이트 정수라는 가정입니다.
 
-
-	std::list<int>::size_type = s.size();
-
-
+	// list 의 크기를 담을때 사용하는 타입은
+	// list 설계자가 정확히 알고 있습니다.
+	std::list<int>::size_type sz = s.size();
+	auto sz2 = s.size(); // C++11 부터는 이 코드 가능
 }
+// cppreference.com 에서 "std::list" 검색해 보세요.

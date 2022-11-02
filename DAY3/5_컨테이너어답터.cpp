5248403220ab37a4f514b2791f521986f68ce671
ecourse <smkang@codenuri.co.kr>
2022-11-02 15:13:52 +0900

    Add files via upload

--- DAY3/5_컨테이너어답터.cpp
+++ DAY3/5_컨테이너어답터.cpp
@@ -12,20 +12,27 @@
 
 // list 의 멤버 함수 이름을 변경해서 스택처럼 보이게 하자.
 // "push_back 함수" => "push 함수" 로 이름 변경한것
-template<typename T> class stack
+
+// adapter 패턴 : "인터페이스의 불일치를 해결"하는 패턴
+// => 기존 클래스의 인터페이스(함수이름)을 변경해서 사용자가 요구하는
+//    클래스를 만드는 것.
+
+template<typename T, typename C = std::deque<T> > class stack
 {
-	std::list<T> c;
+	C c;
 public:
-	void push(const T& value) { c.push_back(value); }
-	void pop()                { c.pop_back(); }
-	T& top()                  { return c.back(); }
+	inline void push(const T& value) { c.push_back(value); }
+	inline void pop()                { c.pop_back(); }
+	inline T& top()                  { return c.back(); }
 };
-
-
 
 int main()
 {
-	stack<int> s;
+	stack<int, std::list<int>>   s1;
+	stack<int, std::vector<int>> s2; // vector 를 스택처럼 사용하겠다는 의미
+
+	stack<int> s;	// 디폴트 값인 "std::deque" 를 스택처럼 사용하겠다는 의미
+
 	s.push(10);
 	s.push(20);
 

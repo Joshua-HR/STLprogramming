7022163c40994b5ab19cdc041ab7345e159112ab
ecourse <smkang@codenuri.co.kr>
2022-11-02 15:59:29 +0900

    Add files via upload

--- DAY3/5_컨테이너어답터3-2.cpp
+++ DAY3/5_컨테이너어답터3-2.cpp
@@ -1,8 +1,30 @@
 #include <vector>
 
+/*
 class queue1
 {
 	std::vector<int> q;
+public:
+	void pop()
+	{
+		q.pop_front();	// 항상 에러
+						// vector 는 pop_front() 없습니다.
+	}
+};
+
+int main()
+{
+	queue1 q1;
+
+	// queue1 은 템플릿(틀)이 아닙니다. 
+	// q1.pop() 을 사용하지 않아도 에러입니다.
+}
+*/
+
+template<typename T>
+class queue1
+{
+	std::vector<T> q;
 public:
 	void pop()
 	{
@@ -12,5 +34,7 @@ public:
 
 int main()
 {
-	queue1 q1;
+	queue1<int> q1;
+
+	q1.pop();
 }

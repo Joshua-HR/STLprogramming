0c7d69c84b41b787c6efa0fe28a7005c1311c516
ecourse <smkang@codenuri.co.kr>
2022-11-03 09:14:11 +0900

    Add files via upload

---
+++ DAY4/1_유틸리티3-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include <fstream>  
+#include <sstream>  
+#include <string>
+
+int main()
+{
+	std::ostringstream oss; // 메모리(문자열) 출력 스트림
+	oss << "hello";
+
+	std::istringstream iss("to be or not to be");
+
+	std::string s;
+//	iss >> s;	// "to" 즉, iss 가 가진 문자열에서 1번째 단어
+
+	while (iss >> s)
+		std::cout << s << std::endl;
+
+}

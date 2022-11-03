d0265a897a08802c2e1294cd90dd18b2832ce07a
ecourse <smkang@codenuri.co.kr>
2022-11-03 09:00:51 +0900

    Add files via upload

---
+++ DAY4/1_유틸리티3.cpp
@@ -0,0 +1,21 @@
+// C++은 3개의 표준 스트림이 있습니다.
+#include <iostream> // 1. 표준 입출력 스트림
+#include <fstream>  // 2. 화일 스트림
+#include <sstream>  // 3. 메모리(스트링)스트림
+#include <string>
+
+// 핵심 1. 표준 입출력 스트림은 객체가 이미 생성되어 있습니다.
+// ostream cout;
+// istream cin;
+
+int main()
+{
+	// 핵심 2. 파일 입출력 스트림 객체는 사용자가 만들어서 사용하면됩니다.
+	// => cout, cin 과 사용법 동일
+	std::ofstream fout("a.txt");
+
+
+	std::cout << "hello";	// 표준 출력에 출력
+	fout	  << "hello";	// 파일로 출력
+
+}

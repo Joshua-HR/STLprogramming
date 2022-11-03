8ba75f7ff97d757229ec8bbf5eb9aac0ca325e65
ecourse <smkang@codenuri.co.kr>
2022-11-03 14:22:33 +0900

    Add files via upload

--- DAY4/8_스마트포인터1.cpp
+++ DAY4/8_스마트포인터1.cpp
@@ -7,8 +7,13 @@ public:
 	~Car()    { std::cout << "~Car" << std::endl; }
 };
 // 개념 : 임의 타입의 객체가 다른 타입의 포인터 역활을 하는 것
-// 원리 : 
-// 장점 : 
+// 
+// 원리 : -> 와 * 연산자를 재정의 해서 포인터처럼 동작하게 만든것
+// 
+// 장점 : raw pointer 가 아닌 객체라는 점
+//       생성/복사/대입/파괴의 모든 순간에 약속된 함수가 호출된다.
+//       각 과정에서 추가의 작업수행가능
+//       대표적 활용이 소멸자에서 delete 기능 
 
 // 아래 Ptr 클래스가 핵심 입니다.
 class Ptr
@@ -16,6 +21,8 @@ class Ptr
 	Car* obj;
 public:
 	Ptr(Car* p = 0) : obj(p) {}
+	
+	~Ptr() { delete obj; }
 
 	Car* operator->() { return obj; }
 	Car& operator*() { return *obj; }

be34c8483618db426e6b9afa75b42aa21f89a275
ecourse <smkang@codenuri.co.kr>
2022-11-03 11:39:23 +0900

    Add files via upload

--- DAY4/6_사용자정의타입.cpp
+++ DAY4/6_사용자정의타입.cpp
@@ -20,6 +20,18 @@ int main()
 								  // "복사 생성자" 사용
 
 	std::vector<Point> v1(10, Point(0,0));
+
+
+	// resize 등을 할때도 객체를 전달해야 합니다.
+	v1.resize(20, Point(0, 0));
+			// => 앞의 10개는 기존 버퍼의 내용으로 복사 생성
+			// => 새로운 10개는 "Point(0,0)" 객체로 복사 생성.
+
+	// 참고, Point 와 Point* 를 잘 구별하세요
+//	std::vector<Point>  v3(10);	// 객체 자체를 보관
+								// 디폴트 생성자가 없으므로 error
+	
+	std::vector<Point*> v4(10); // 객체를 가리키는 주소(포인터)보관
 }
 
 

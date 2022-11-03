a4beea133d3b6939dbbabf9992d3a370d828cdd1
ecourse <smkang@codenuri.co.kr>
2022-11-03 09:10:56 +0900

    Add files via upload

--- DAY4/1_유틸리티3-1.cpp
+++ DAY4/1_유틸리티3-1.cpp
@@ -9,4 +9,19 @@ int main()
 
 	// pi를 사용해서 문자열("pi = 3.141592") 로 만들려고 합니다.
 	// C 방식
+//	char s[32];
+//	sprintf(s, "pi = %f", pi);  // pi 값에 따라 
+								// 버퍼 오버플로우 가능성 있습니다.
+
+//	snprintf(s, 32, "pi = %f", pi);	// 버퍼오버플로우는 없지만
+									// 데이타 손실 
+							
+	// C++ 스타일
+	std::ostringstream oss;
+
+	oss << "pi = " << pi;	// cout 과 동일한 사용법
+
+	std::string s = oss.str();
+
+	std:cout << s << std::endl;
 }

38da835b516543e0c532a35cae1698d0871aebe0
ecourse <smkang@codenuri.co.kr>
2022-11-03 14:10:01 +0900

    Add files via upload

---
+++ DAY4/컨테이너정리.cpp
@@ -0,0 +1,24 @@
+// 컨테이너정리
+// 선형컨테이너 : 5개
+
+// C++98 : std::vector, std::list, std::deque
+// C++11 : std::array,  std::forward_list
+
+// 컨테이너 어답터 3개
+// C++ 98 : stack, queue, priority_queue
+
+// associative(연관 컨테이너)
+// tree 기반 : set, map 
+// hash 기반 : unordered_set, unordered_map - C++11
+
+// 그래프는 아직 없습니다.
+
+// C++98 STL : 자료구조와 알고리즘 위주의 라이브러리
+//             Iterator, Algorithm, Container
+
+// C++11 STL : 자료구조와 알고리즘 추가
+//             스마트 포인터, function, chrono, thread 등의 유틸리티
+// 
+// C++14, 17 : 유틸리티 추가!!
+// 
+// C++20     : Range 추가

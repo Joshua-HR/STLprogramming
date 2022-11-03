d1c6b2a8b8936dfb66adbb77251693cd066a03eb
ecourse <smkang@codenuri.co.kr>
2022-11-03 10:43:17 +0900

    Add files via upload

---
+++ DAY4/3_map2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <list>
+#include <map>
+#include <algorithm>
+
+// 예제. 
+
+int main()
+{
+	std::map<std::string, std::list<int>> indexMap; // 핵심
+										// 구조 잘생각해 보세요
+
+	std::ifstream fin("3_map2.cpp"); // 현재 파일 이름
+	
+	std::string s;
+	int no = 0;
+
+	while (std::getline(fin, s))
+	{
+
+	}
+}
+
+

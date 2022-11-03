77fe042e06541e45070b4eea52c05472d67d5a4d
ecourse <smkang@codenuri.co.kr>
2022-11-03 10:51:51 +0900

    Add files via upload

--- DAY4/3_map2.cpp
+++ DAY4/3_map2.cpp
@@ -5,7 +5,7 @@
 #include <list>
 #include <map>
 #include <algorithm>
-
+#include "show.h"
 // 예제. 
 
 int main()
@@ -18,9 +18,26 @@ int main()
 	std::string s;
 	int no = 0;
 
-	while (std::getline(fin, s))
+	while (std::getline(fin, s))	// 한 줄 읽기
 	{
+		++no;
+		std::istringstream iss(s);
+		std::string word;
 
+		while (iss >> word) // 한줄 => 단어로 분리
+		{
+			// 키값(word) 이 있으면 value(list) 반환
+			// 없어도 "신규로 만들고" value(list)반환
+			indexMap[word].push_back(no);
+		}
+	}
+	//=======================================
+	// map 의 모든 항목 출력
+	for (const auto& p : indexMap)
+	{
+		// p 는 결국 pair 의 참조 입니다.
+		std::cout << p.first << " : ";
+		show(p.second);	// p.second 는 list<int>
 	}
 }
 

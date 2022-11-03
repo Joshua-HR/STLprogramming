80aa0c3ef9cd9ab2d22bdce994c7ab33b91e0815
ecourse <smkang@codenuri.co.kr>
2022-11-03 09:18:59 +0900

    Add files via upload

--- DAY4/1_유틸리티3-2.cpp
+++ DAY4/1_유틸리티3-2.cpp
@@ -17,4 +17,8 @@ int main()
 	while (iss >> s)
 		std::cout << s << std::endl;
 
+	// C++23 에서 std::print 나옵니다
+	// 아래처럼 사용합니다. cout 보다 빠르고, 편리합니다.
+//	std::print("n = {0}, s = {1}", 10, "AA");
+//	std::println("n = {0}, s = {1}", 10, "AA");
 }

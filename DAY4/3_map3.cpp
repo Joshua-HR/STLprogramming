08149d1f23a2d904ac1ce11782e84660d8c6ebf7
ecourse <smkang@codenuri.co.kr>
2022-11-03 10:33:57 +0900

    Add files via upload

--- DAY4/3_map3.cpp
+++ DAY4/3_map3.cpp
@@ -1,11 +1,18 @@
 #include <map>
 
+struct Point
+{
+	int x, y;
+};
+
 void foo(int n) {}
-void goo(std::pair<int, int> p) {}
+void goo(Point pt) {}
+void hoo(std::pair<int, int> p) {}
 
 int main()
 {
 	int n1 = 10;
 	int n2 = { 10 }; // C++11. 중괄호 초기화 문법
+
 
 }

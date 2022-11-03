f9ffe4f22719cac9d40825c94b31f2e8084f144e
ecourse <smkang@codenuri.co.kr>
2022-11-04 08:41:04 +0900

    Add files via upload

--- DAY5/1_bind_function1.cpp
+++ DAY5/1_bind_function1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <functional>
+using namespace std::placeholders; // _1, _2, _3 ... 을 위해서
 
 // 4항 함수
 void foo( int a, int b, int c, int d)
@@ -14,6 +15,14 @@ int main()
 
     // C++11 std::bind : M 항 함수를 N항 함수로 변경(M > N )
     // => 인자를 고정한 새로운 함수를 생성
+    // => 사용법 : std::bind(M항함수, M개인자)
+    
+    std::bind(foo, 1, 2, 3, 4)();       // 4항 함수 => 0항 함수로 변경
+    std::bind(foo, 3, _1, 7, _2)(9, 8); // foo(3, 9, 7, 8) 의 효과
+    //|<-----  새로운 함수 ----->|        // 4항 함수 => 2항 함수로 변경   
 
-    std::bind(foo, 1, 2, 3, 4)();
+
+    std::bind(foo, ? , ? , ? , ? )(9, 6, 3); // 3, 9, 5, 6 나오게 
+                                             // ? 채워 보세요
+
 }

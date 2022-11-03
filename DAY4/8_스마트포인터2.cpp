d2350a032875c61ac2e24b84c82fd4248d852374
ecourse <smkang@codenuri.co.kr>
2022-11-03 14:37:05 +0900

    Add files via upload

---
+++ DAY4/8_스마트포인터2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Ptr
+{
+	Car* obj;
+public:
+	Ptr(Car* p = 0) : obj(p) {}
+
+	~Ptr() { delete obj; }
+
+	Car* operator->() { return obj; }
+	Car& operator*() { return *obj; }
+};
+
+int main()
+{
+}
+
+

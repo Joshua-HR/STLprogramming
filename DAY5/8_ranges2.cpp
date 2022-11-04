b8f618d27aa07c394b0a0671ea22f1df9278459d
ecourse <smkang@codenuri.co.kr>
2022-11-04 16:05:24 +0900

    Add files via upload

--- DAY5/8_ranges2.cpp
+++ DAY5/8_ranges2.cpp
@@ -16,15 +16,27 @@ public:
 	auto begin() { return range.begin(); }
 	auto end()   { return range.begin() + count; }
 };
+template<typename T>
+class reverse_view
+{
+	T& range;
+public:
+	reverse_view(T& r) : range(r){}
+
+	auto begin() { return range.rbegin(); }
+	auto end() { return range.rend(); }
+};
+
 
 int main()
 {
 	std::vector<int> v = {1,2,3,4,5,6,7,8,9,10 };
 
-	take_view tv(v, 5); // C++17 이후, 타입 인자 생략가능
-
+//	take_view tv(v, 5); // C++17 이후, 타입 인자 생략가능
 //	for (auto n : v) 
-	for (auto n : tv)
+//	for (auto n : tv)
+//	for (auto n : take_view(v, 5))
+	for (auto n : reverse_view(v))
 		std::cout << n << ", ";
 
 }

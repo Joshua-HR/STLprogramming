051aeff58ecc950107f15ddcb9bf8d4c45890a5e
ecourse <smkang@codenuri.co.kr>
2022-11-04 13:40:42 +0900

    Add files via upload

--- DAY5/4_traits3.cpp
+++ DAY5/4_traits3.cpp
@@ -2,21 +2,27 @@
 #include <iostream>
 #include <type_traits>
 
-template<typename T> void printv(const T& v)
+template<typename T> 
+void printv(const T& v)
 {
-	if ( std::is_pointer<T>::value )
+//	if ( std::is_pointer<T>::value )
+
+	// if constexpr : 컴파일 시간 제어문 ( C++17)
+	//				조건이 false 라면 
+	//				"템플릿" => "C++ 코드" 생성 안됨
+	if constexpr (std::is_pointer<T>::value)
 		std::cout << v << " : " << *v << std::endl;
 	else
 		std::cout << v << std::endl;
 }
 
+
 int main()
 {
 	int n = 10;
-	double d = 10;
 
-	printv(n);
-	printv(d);
-	printv(&n);
+	printv(&n);// ok
+
+	printv(n); // 이코드가 있으면 error
 }
 

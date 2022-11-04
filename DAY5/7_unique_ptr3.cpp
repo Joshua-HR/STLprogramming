86ca6f1cf14fc77db70ca2257c1fef204cb2ac93
ecourse <smkang@codenuri.co.kr>
2022-11-04 15:54:09 +0900

    Add files via upload

--- DAY5/7_unique_ptr3.cpp
+++ DAY5/7_unique_ptr3.cpp
@@ -18,9 +18,15 @@ int main()
 
 
 	// shared_ptr vs unique_ptr
-	std::shared_ptr<int> sp(malloc(100), free);
-	std::unique_ptr<int, Freer > up(malloc(100));
-	
+	// shared_ptr : 삭제자를 변경해도 같은 타입
+	//				단, 삭제자를 보관할 공간이 필요!
+	std::shared_ptr<int> sp1(malloc(100), free);
+	std::shared_ptr<int> sp2(malloc(100), foo);
+
+	// unique_ptr : 삭제자를 변경하면 다른 타입
+	//			    삭제자를 저장할 공간 필요 없다.
+	std::unique_ptr<int, Freer1 > up1(malloc(100));
+	std::unique_ptr<int, Freer2 > up2(malloc(100));
 
 
 }

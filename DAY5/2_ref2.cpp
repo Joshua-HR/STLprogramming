2ed2cc2d2ea8979c0ac63909c266da3709716a8b
ecourse <smkang@codenuri.co.kr>
2022-11-04 09:57:19 +0900

    Add files via upload

--- DAY5/2_ref2.cpp
+++ DAY5/2_ref2.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <functional>
 
+// reference_wrapper 핵심
+// 1. 객체의 주소를 보관하고 있습니다.
+// 2. raw reference 로 변환 가능합니다.
+// 3. 복사 및 대입은 디폴트 버전 사용!!
+
+
+// C++20 에서 "std::ref_view" 가 추가되는데.. 
+// => reference_wrapper 를 발전 시킨 개념입니다.
+
+
 // C++ 표준의 reference_wrapper 아주 간단한 도구 입니다.
 template<typename T>
 class reference_wrapper
@@ -12,6 +22,9 @@ public:
 	// "raw reference" 로의 변환을 위한 변환 연산자
 	operator T& () { return *pobj; }
 };
+
+
+
 
 int main()
 {

8cf8614699a2796f1071575824b39d0575191ee9
ecourse <smkang@codenuri.co.kr>
2022-11-04 12:02:03 +0900

    Add files via upload

--- DAY5/5_string_view4.cpp
+++ DAY5/5_string_view4.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
 #include <string>
 
+// 약속된 멤버 함수를 만들면 됩니다.
+// => static 멤버 함수 몇개를 만들어야 된다고 표준에 정리되어 있습니다.
+// => cppreference.com 에서 std::char_traits 검색하세요
+//    예제 있습니다   ( std::basic_string 을 먼저 검색해 보세요)
+// 
 // 문자열 비교에 사용할 단위 전략
-struct ci_char_traits
+struct ci_char_traits : public std::char_traits<char>
 {
-	// 약속된 멤버 함수를 만들면 됩니다.
-	// => static 멤버 함수 몇개를 만들어야 된다고 표준에 정리되어 있습니다.
+    static char to_upper(char ch) {
+        return std::toupper((unsigned char)ch);
+    }
+    static bool eq(char c1, char c2) {
+        return to_upper(c1) == to_upper(c2);
+    }
+    static bool lt(char c1, char c2) {
+        return to_upper(c1) < to_upper(c2);
+    }
+    static int compare(const char* s1, const char* s2, std::size_t n) {
+        while (n-- != 0) {
+            if (to_upper(*s1) < to_upper(*s2)) return -1;
+            if (to_upper(*s1) > to_upper(*s2)) return 1;
+            ++s1; ++s2;
+        }
+        return 0;
+    }
+    static const char* find(const char* s, std::size_t n, char a) {
+        auto const ua(to_upper(a));
+        while (n-- != 0)
+        {
+            if (to_upper(*s) == ua)
+                return s;
+            s++;
+        }
+        return nullptr;
+    }
 };
+
+
 int main()
 {
 	std::string s1 = "abcd";

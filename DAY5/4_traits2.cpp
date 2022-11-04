557f38a1022a8c9e80aeba86523baeb0964669af
ecourse <smkang@codenuri.co.kr>
2022-11-04 12:14:14 +0900

    Add files via upload

--- DAY5/4_traits2.cpp
+++ DAY5/4_traits2.cpp
@@ -6,20 +6,25 @@ template<typename T> struct is_pointer
 	enum { value = false };
 };
 
+// template 부분 특수화 라는 문법. 
+// 특정 조건을 만족할때 구현을 변경하겠다는 문법.
+template<typename T> struct is_pointer<T*>
+{
+	enum { value = true };
+};
+
 template<typename T> void foo(const T& b)
 {
-	if ( is_pointer<T>::value  )
+	// T : int, int*
+	if (is_pointer<T>::value)
 		std::cout << "포인터" << std::endl;
 	else
 		std::cout << "포인터 아님" << std::endl;
-
 }
-
 int main()
 {
 	int n = 10;
-
-	printv(n);
-	printv(&n);
+	foo(n);
+	foo(&n);
 }
 
